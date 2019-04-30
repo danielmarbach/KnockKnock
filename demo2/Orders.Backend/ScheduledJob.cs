72fa3e906c352c5ebf76014e479850aea8e4a299
danielmarbach <daniel.marbach@openplace.net>
2019-04-30 22:46:33 +0200

    Regions to hide output

--- demo2/Orders.Backend/ScheduledJob.cs
+++ demo2/Orders.Backend/ScheduledJob.cs
@@ -7,21 +7,28 @@ namespace Orders.Backend
     {
         public static void Run() 
         {
+            #region
             Console.WriteLine("Running nightly batch for all customers");
+            #endregion
             foreach (var customer in Database.GetCustomers())
             {
-                var totalOfAllOrdersOfLastWeek = Database.LastWeekOrdersFor(customer.CustomerId)
+                var totalOfAllOrdersOfLastWeek = Database
+                    .LastWeekOrdersFor(customer.CustomerId)
                     .Sum(o => o.Total);
 
                 if (totalOfAllOrdersOfLastWeek >= 500)
                 {
+                    #region
                     Console.WriteLine($"Customer {customer.ShortId} will get discount.");
+                    #endregion
                     customer.GiveDiscount = true;
 
                     Database.Save(customer);
                 }
             }
+            #region
             Console.WriteLine("Done with nightly batch for all customers");
+            #endregion
         }
     }
 }
